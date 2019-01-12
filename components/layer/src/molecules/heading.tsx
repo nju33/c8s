2a380ce5225b685857fac3b1a607f3355c3bdaa7
nju33 <nju33.ki@gmail.com>
2019-01-12 17:00:34 +0900

    chore(layout): remove default styles

--- components/layer/src/molecules/heading.tsx
+++ components/layer/src/molecules/heading.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {PayloadContext} from '../payload';
-import {H} from '../atoms';
-import styled from '@c8s/theme';
 
 export const Heading: React.SFC<React.HTMLAttributes<HTMLHeadingElement>> = ({
   children,
@@ -12,26 +10,13 @@ export const Heading: React.SFC<React.HTMLAttributes<HTMLHeadingElement>> = ({
       {({level}) => {
         if (level > 6) {
           return (
-            <H
-              {...headlingProps}
-              role="heading"
-              aria-level={level}
-            >
+            <div {...headlingProps} role="heading" aria-level={level}>
               {children}
-            </H>
+            </div>
           );
         }
-
-        const tagName = `h${level}` as 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-        const Component = styled(H.withComponent(tagName))``;
-        return (
-          <Component
-            {...headlingProps}
-            aria-level={level}
-          >
-            {children}
-          </Component>
-        );
+        const Tag = `h${level}` as 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+        return <Tag {...headlingProps}>{children}</Tag>;
       }}
     </PayloadContext.Consumer>
   );
